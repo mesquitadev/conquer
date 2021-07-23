77596f94657d36d09a7c3c3f5e128099574c0992
Victor Mesquita <mesquitadev@gmail.com>
2021-07-23 00:35:27 -0300

    ïnitial

--- index-3.php
+++ index-3.php
@@ -7,7 +7,7 @@
 <center><font color="#000000" size="3"><div align="center">Bem Vindo a Loja Virtual Do <?php echo $servername; ?></div></font></center>
 </table></tr>
 <table width="550" height="0" border="0" align="center"><tr>
-<td width="*"><div align="center"><strong><a href="index1.php" tppabs="index1.php"><img src="img/Inicio.png" tppabs="img/Inicio.png"></a></strong></div></td>
+<td width="*"><div align="center"><strong><a href="index1.htm" tppabs="index1.htm"><img src="img/Inicio.png" tppabs="img/Inicio.png"></a></strong></div></td>
 </table><br />
 
 <div align="center"><table width="95%" align="center" border="0" cellpadding="0" cellspacing="0">
@@ -17,7 +17,7 @@
 <div align="center"><img src="img/set.gif" tppabs="img/set.gif" alt="" width="10" height="10" /><strong> <font size="2">Concede ao us&uacute;ario</strong>:<br /></font></div>
 <div align="center"><img src="img/smile_azul.gif" tppabs="img/smile_azul.gif" alt="" width="10" height="10" /><font size="2"><font color="Black"> 500.000 Cps</font> <br></div>
 <div align="center"><img src="img/bolinha_azul.png" tppabs="img/bolinha_azul.png" alt="" width="7" height="7" /> <strong><font color="#000000 size="2">Pre&ccedil;o</strong>:<font color="#FF0000"><font color="Black">R$ 30,00</strike></font><br /></div> </p></font></div>
-<div align="center"><a href="deposito.php" tppabs="deposito.php"><img src="img/comprar-1.png" tppabs="img/comprar.png"> </p></font></div>
+<div align="center"><a href="deposito.htm" tppabs="deposito.htm"><img src="img/comprar-1.png" tppabs="img/comprar.png"> </p></font></div>
 
 </form></th>
 
@@ -26,7 +26,7 @@
 <div align="center"><img src="img/set.gif" tppabs="img/set.gif" alt="" width="10" height="10" /><strong> <font size="2">Concede ao us&uacute;ario</strong>:<br /></font></div>
 <div align="center"><img src="img/smile_azul.gif" tppabs="img/smile_azul.gif" alt="" width="10" height="10" /><font size="2"><font color="Black"> 1.000.000 Cps</font> <br></div>
 <div align="center"><img src="img/bolinha_azul.png" tppabs="img/bolinha_azul.png" alt="" width="7" height="7" /> <strong><font color="#000000 size="2">Pre&ccedil;o</strong>:<font color="#FF0000"><font color="Black">R$ 50,00</strike></font><br /></div>
-<div align="center"><a href="deposito.php" tppabs="deposito.php"><img src="img/comprar-1.png" tppabs="img/comprar.png"> </p></font></div>
+<div align="center"><a href="deposito.htm" tppabs="deposito.htm"><img src="img/comprar-1.png" tppabs="img/comprar.png"> </p></font></div>
 
 </form></th>
 
@@ -47,21 +47,21 @@
 <div align="center"><img src="img/set.gif" tppabs="img/set.gif" alt="" width="10" height="10" /><strong> <font size="2"></strong><br /></font></div>
 <div align="center"><img src="img/smile_azul.gif" tppabs="img/smile_azul.gif" alt="" width="10" height="10" /><font size="2"><font color="Black"> Pack Garment Exclusiva (Unidade)</font> <br></div>
 <div align="center"><img src="img/bolinha_azul.png" tppabs="img/bolinha_azul.png" alt="" width="7" height="7" /> <strong><font color="#000000 size="2">Pre&ccedil;o</strong>:<font color="#FF0000"><font color="Black">R$ 15,00</strike></font><br /></div>
-<div align="center"><a href="deposito.php" tppabs="deposito.php"><img src="img/comprar-1.png" tppabs="img/comprar.png"> </p></font></div>
+<div align="center"><a href="deposito.htm" tppabs="deposito.htm"><img src="img/comprar-1.png" tppabs="img/comprar.png"> </p></font></div>
 
 </form></th>
 <div align="center"><img src="img/Documentos.png" tppabs="img/Documentos.png"></a></font><br />
 <div align="center"><img src="img/set.gif" tppabs="img/set.gif" alt="" width="10" height="10" /><strong> <font size="2"></strong><br /></font></div>
 <div align="center"><img src="img/smile_azul.gif" tppabs="img/smile_azul.gif" alt="" width="10" height="10" /><font size="2"><font color="Black"> Level 133 </font> <br></div>
 <div align="center"><img src="img/bolinha_azul.png" tppabs="img/bolinha_azul.png" alt="" width="7" height="7" /> <strong><font color="#000000 size="2">Pre&ccedil;o</strong>:<font color="#FF0000"><font color="Black">R$ 30,00</strike></font><br /></div>
-<div align="center"><a href="deposito.php" tppabs="deposito.php"><img src="img/comprar-1.png" tppabs="img/comprar.png"> 
+<div align="center"><a href="deposito.htm" tppabs="deposito.htm"><img src="img/comprar-1.png" tppabs="img/comprar.png"> 
 
 <th scope="col"><font size="2">
 <div align="center"><img src="img/download.png" tppabs="img/download.png"></a></font><br />
 <div align="center"><img src="img/set.gif" tppabs="img/set.gif" alt="" width="10" height="10" /><strong> <font size="2"></strong><br /></font></div>
 <div align="center"><img src="img/smile_azul.gif" tppabs="img/smile_azul.gif" alt="" width="10" height="10" /><font size="2"><font color="Black"> Gold Prize</font> <br></div>
 <div align="center"><img src="img/bolinha_azul.png" tppabs="img/bolinha_azul.png" alt="" width="7" height="7" /> <strong><font color="#000000 size="2">Pre&ccedil;o</strong>:<font color="#FF0000"><font color="Black">R$ 40,00</strike></font><br /></div>
-<div align="center"><a href="deposito.php" tppabs="deposito.php"><img src="img/comprar-1.png" tppabs="img/comprar.png"> </p></font></div>
+<div align="center"><a href="deposito.htm" tppabs="deposito.htm"><img src="img/comprar-1.png" tppabs="img/comprar.png"> </p></font></div>
 
 </form></th></tr></table></div>
 </p></td></tr></table></p>
